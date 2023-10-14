99c68408dc9cbbba8120d2c7bb831b7324700b05
_drstrange <prudhvirajboddu@gmail.com>
2023-10-14 20:30:17 +0000

    Error in locs of x and y

--- train.py
+++ train.py
@@ -2,8 +2,9 @@ import os
 import pandas as pd
 import torch
 from torchvision import transforms
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
+
 
 class FaceDataset(Dataset):
     def __init__(self, csv_file, img_dir, transform=None):
@@ -13,52 +14,58 @@ class FaceDataset(Dataset):
 
     def __len__(self):
         return len(self.annotations)
-    
+
     def __getitem__(self, index):
-        label_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
+        label_path = os.path.join(
+            self.img_dir, self.annotations.iloc[index, 0])
         image = Image.open(label_path).convert('RGB')
-        
-        ymin = self.annotations.iloc[index, 1] 
+
+        ymin = self.annotations.iloc[index, 1]
         xmin = self.annotations.iloc[index, 2]
         ymax = self.annotations.iloc[index, 3]
         xmax = self.annotations.iloc[index, 4]
         boxes = [[xmin, ymin, xmax, ymax]]
-        
+
         if self.transform:
             image = self.transform(image)
-            
+
         target = {}
         target['boxes'] = boxes
-        target['labels'] = torch.tensor([1]) 
+        target['labels'] = torch.tensor([1])
 
         return image, target
-    
+
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.ToTensor(transforms.PILToTensor()),
+        # transforms.PILToTensor(),
         transforms.RandomHorizontalFlip(),
-        transforms.Scale((224,224)),
-        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
-        transforms.RandomRotation(5, resample=False,expand=False, center=None),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Scale((224,224)),
-       transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
-        transforms.RandomRotation(5, resample=False,expand=False, center=None),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ]),
+        # transforms.ColorJitter(
+        #     brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+        # transforms.RandomRotation(
+        #     5, resample=False, expand=False, center=None),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    # 'val': transforms.Compose([
+    #     transforms.PILToTensor(),
+    #     transforms.Scale((224, 224)),
+    #     transforms.ColorJitter(
+    #         brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+    #     transforms.RandomRotation(
+    #         5, resample=False, expand=False, center=None),
+    #     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    # ]),
 }
-    
 
 
-dataset = FaceDataset('train_labels.csv','train',transform = transforms.Compose([transforms.PILToTensor()]))
+dataset = FaceDataset('train_labels.csv', 'train',
+                      transform=data_transforms['train'])
 
-train_loader = DataLoader(dataset,shuffle=True)
+train_loader = DataLoader(dataset, shuffle=True)
 
-for images,targets in train_loader:
+for images, targets in train_loader:
     print(images.shape)
     print(targets)
     break
