0fbfb7356002e1140cd8ab776ddc087415deb9ca
prudhvirajboddu <prudhvirajboddu@gmail.com>
2023-11-06 18:34:21 +0000

    using tripletmarginwithdistance loss function

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import torchvision
+import torch
+from torch import nn
 
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
 
 def create_model(num_classes):
     
@@ -13,3 +16,18 @@ def create_model(num_classes):
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
 
     return model
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        
+    def calc_euclidean(self, x1, x2):
+        return (x1 - x2).pow(2).sum(1)
+    
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
+        distance_positive = self.calc_euclidean(anchor, positive)
+        distance_negative = self.calc_euclidean(anchor, negative)
+        losses = torch.relu(distance_positive - distance_negative + self.margin)
+
+        return losses.mean()
