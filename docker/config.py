dddc7862c7b3550890120aed1b18568a548ac615
prudhvirajboddu <prudhvirajboddu@gmail.com>
2023-11-30 13:56:43 -0500

    Adding app to docker

---
+++ docker/config.py
@@ -0,0 +1,9 @@
+RESIZE_TO = 512 # resize the image for training and transforms
+
+# classes: 0 index is reserved for background
+CLASSES = [
+    'student','Security', 'Staff', 'Facility Worker','Food Service worker'
+]
+
+# number of classes
+NUM_CLASSES = 5
