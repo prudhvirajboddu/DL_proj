9f92696d1e9576065fb2a0eb23325f43ad9434de
_drstrange <prudhvirajboddu@gmail.com>
2023-11-30 16:01:28 -0500

    Added code to retain the pretrained weights

--- model.py
+++ model.py
@@ -9,6 +9,10 @@ def create_model(num_classes):
     
     # load Faster RCNN pre-trained model
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights = "DEFAULT" ,pretrained=True, pretrained_backbone=True, progress=True, trainable_backbone_layers=4)
+
+    #setting pretrained parameters = False
+    for param in model.parameters():
+        param.requires_grad = False
     
     # get the number of input features 
     in_features = model.roi_heads.box_predictor.cls_score.in_features
